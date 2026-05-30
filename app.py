20b68d3f89f41e2c60057236d3d8f6adbb695a93
Leonard Duga <batoduga2001@gmail.com>
2026-05-30 14:20:41 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import numpy as np
+import tensorflow as tf
 from PIL import Image
-import pickle
 
-# Load model (NO tensorflow)
-model = pickle.load(open("model.pkl", "rb"))
+model = tf.keras.models.load_model("flower_resnet50.keras")
 
 class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
 st.title("🌸 Flower Classifier")
 
-uploaded_file = st.file_uploader("Upload Image", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload Image")
 
 if uploaded_file:
     image = Image.open(uploaded_file)
     st.image(image)
 
-    st.write("⚠ Model requires preprocessing pipeline (you must match training)")
+    img = image.resize((224, 224))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
 
-    st.success("Prediction system ready (fix model format needed)")
+    prediction = model.predict(img)
+    label = class_names[np.argmax(prediction)]
+
+    st.success(f"Prediction: {label}")
